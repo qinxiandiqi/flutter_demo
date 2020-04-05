c11ad27a1cec8aac65d4a833fb734629a68fd3e7
Jianan <qinxiandiqi@foxmail.com>
2020-04-05 15:54:39 +0800

    init project

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:my_app/RandomWorks.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Startup Name Generator',
+        theme: new ThemeData(primaryColor: Colors.red),
+        home: RandomWords());
+  }
+}
